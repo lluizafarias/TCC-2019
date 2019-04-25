44df0a421dfa62306dd11fa749e7216f5f421a6a
qrlist <49570994+qrlist@users.noreply.github.com>
2019-04-25 10:45:06 -0300

    Add files via upload

---
+++ QrList/cadastro.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <!-- Standard Meta -->
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+
+ <!-- Site Properties -->
+  <title>QrList</title>
+  <link rel="stylesheet" type="text/css" href="semantic/dist/semantic.min.css">
+  <script
+    src="https://code.jquery.com/jquery-3.1.1.min.js"
+    integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
+    crossorigin="anonymous"></script>
+  <script src="semantic/dist/semantic.min.js"></script>
+    <style type="text/css">
+    body {
+      background-color: #DADADA;
+    }
+    </style>
+
+<body>
+	
+	<div class="ui grid">
+	  <div class="row">
+		<div class="four wide column"></div>
+		<div class="eight wide column">
+		  <div class="ui middle aligned aligned grid">		  
+			  
+		  <div class="column">
+			<h2 class="ui teal image header">
+			  <img src="assets/images/logo.png" class="image">
+			  <div class="content">
+				Criar conta!
+			  </div>
+			</h2>
+			<form class="ui large form">
+			  <div class="ui stacked segment">
+				<div class="field">
+				  <label>Primeiro nome</label>
+				  <input type="text" placeholder="Primeiro Nome">
+				</div>
+				<div class="field">
+				  <label>Sobrenome</label>
+				  <input type="text" placeholder="Sobrenome">
+				</div>
+				<div class="field">
+				  <label>Telefone</label>
+				  <input type="text" placeholder="Telefone">
+				</div>
+				<div class="field">
+					<label>E-mail</label>
+					<input type="text" placeholder="E-mail">
+				</div>
+				<div class="field">
+				  <label>Senha</label>
+				  <input placeholder="Senha" type="password">
+				</div>
+				<div class="field">
+				  <label>Confirmar Senha</label>
+				  <input placeholder="Confirmar Senha" type="password">
+				</div>
+				<div class="ui fluid large teal submit button">Login</div>
+			  </div>
+
+			  <div class="ui error message"></div>
+
+			</form>
+		  </div>  
+	  </div>
+    <div class="four wide column"></div>
+  </div>
+</div>
+  </div>
+</div>
+</body>
+</html>
